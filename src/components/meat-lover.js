089a2679dbefc79f6970480425d724a6d708eb76
William14S <wsandoval7645@richmondhillhs.org>
2020-11-24 20:56:54 +0000

    done

--- src/components/meat-lover.js
+++ src/components/meat-lover.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import meat from './images/plain.jpg';
+import meat from './images/meat.jpg';
 import './css/slice.css';
 
 function MeatLover() {
@@ -9,10 +9,10 @@ function MeatLover() {
         <img src={ meat } class="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">Plain Slice</h5>
-          <p className="card-text">It's got cheese, it's got tomato sauce, and it's only  a dollar. What more could you ask for?</p>
+          <p className="card-text">It's got cheese, it's got tomato sauce,its got the meats, and it's only  a dollar. Best deal in the world.</p>
         </div>
         <ul className="list-group list-group-flush">
-          <li className="list-group-item">Price: $10.00</li>
+          <li className="list-group-item">Price: $1.00</li>
         </ul>
       </div>
     </div>
