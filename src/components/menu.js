3f2ae2297a67b8f32ec8daa66f592ec7b3ede561
William14S <wsandoval7645@richmondhillhs.org>
2021-01-13 18:26:39 +0000

    some message describing changes

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import Plain from './plain';
-import Pepperoni from './pepperoni';
-import MeatLover from './meat-lover';
-import Veggie from './veggie';
-import Hawaiian from './hawaiian';
-import Bbq from './bbq';
+import Slice from './slice';
+import plain from './images/plain.jpg';
+import pepperoni from './images/pepperoni.jpg';
+import meat from './images/meat.jpg';
+import hawaiian from './images/hawaiian.jpg';
+import bbq from './images/bbq.jpg';
+import veggie from './images/veggie.jpeg';
 import './css/menu.css';
 
 function Menu() {
@@ -14,13 +15,42 @@ function Menu() {
       <h1 className='secondary-text'>Pizza Menu</h1>
       <div className='row'>
 
-        <Plain/>
-        <Pepperoni/>
-        <MeatLover/>
-        <Veggie/>
-        <Hawaiian/>
-        <Bbq/>
-      </div>
+        <Slice
+        img = {plain}
+        title= 'Plain'
+        description = "It's got cheese, it's got tomato sauce, and it's only  a dollar. What more could you ask for?"
+        price = "$1.00"></Slice>
+
+        <Slice
+        img = {pepperoni}
+        title= 'Pepperoni'
+        description = "cheese..yes!Pepperoni yes!Put them together and boom Pepperoni Pizza."
+        price = "$1.00"/>
+
+        <Slice
+        img = {hawaiian}
+        title= 'Hawaiian'
+        description = "We know, you think this is gross. But try this once and we swear you'll be hooked on this pineapple and ham pizza. It will blow your mind !!!"
+        price = "$1.00"/>
+
+        <Slice
+        img = {meat}
+        title= 'Meat'
+        description = "It's got cheese, it's got tomato sauce,its got the meats, and it's only  a dollar. Best deal in the world."
+        price = "$1.00"/>
+
+        <Slice
+        img = {bbq}
+        title= 'Bbq'
+        description = "cheese..yes!BBQ yes!Put them together and boom BBQ Pizza."
+        price = "$1.00"/>
+
+        <Slice
+        img = {veggie}
+        title= 'Veggie'
+        description = "Want to seem healthy? If so then veggie pizza is for you."
+        price = "$1.00"/>
+        </div>
     </div>
   );
 }
